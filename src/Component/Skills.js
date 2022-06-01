d5314df6904833208c586cb661dad2fb6b8492cb
Yuti Shih <yutishih@gmail.com>
2022-06-01 16:52:36 -0400

    1

--- src/Component/Skills.js
+++ src/Component/Skills.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import skills_right from '../Images/skills.jpg'
 
 function Skills() {
+
   return (
     <div className='section-component'>
       <div className='skills'>
@@ -14,36 +15,36 @@ function Skills() {
                 <p>Most common methods for designing websites that work well on desktop is responsive and adaptive design</p>
               </div>
               <div className='progress_bar fadeInUp'>
-                <div className='progress_inner' data-value="85" data-color="#f75023">
+                <div className='progress_inner'>
                   <span>
                     <span className='label opened'>Illustrator</span>
-                    <span className='number'></span>
+                    <span className='number'>100%</span>
                   </span>
                   <div className='background'>
-                    <div className='bar open'>
-                      <div className='bar_in'></div>
+                    <div className='bar'>
+                      <div className='bar_in' style={{width: 100+ "%", backgroundColor: 'rgb(247, 80, 35)'}}></div>
                     </div>
                   </div>
                 </div>
-                <div className='progress_inner' data-value="85" data-color="#f75023">
+                <div className='progress_inner'>
                   <span>
-                    <span className='label opened'>Illustrator</span>
-                    <span className='number'></span>
+                    <span className='label opened'>Photoshop</span>
+                    <span className='number'>100%</span>
                   </span>
                   <div className='background'>
                     <div className='bar open'>
-                      <div className='bar_in'></div>
+                      <div className='bar_in' style={{width: 100 + "%", backgroundColor: 'rgb(28, 190, 89)'}}></div>
                     </div>
                   </div>
                 </div>
-                <div className='progress_inner' data-value="85" data-color="#f75023">
+                <div className='progress_inner'>
                   <span>
-                    <span className='label opened'>Illustrator</span>
-                    <span className='number'></span>
+                    <span className='label opened'>Figma</span>
+                    <span className='number'>100%</span>
                   </span>
                   <div className='background'>
                     <div className='bar open'>
-                      <div className='bar_in'></div>
+                      <div className='bar_in' style={{width: 100 + "%", backgroundColor: 'rgb(128, 103, 240)'}}></div>
                     </div>
                   </div>
                 </div>
