653cb0315b774eae310c0d367cb3de779129fe83
Yuti Shih <yutishih@gmail.com>
2022-06-06 17:00:30 -0400

    1

--- src/Component/Skills.js
+++ src/Component/Skills.js
@@ -1,7 +1,29 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import skills_right from '../Images/skills.jpg'
 
 function Skills() {
+
+  const [barWidth, setBarWidth] = useState(true);
+
+  // scrollPosition is the return value
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    if (position <= 3518){
+      setScrollPosition(3518);
+    }else if (position >= 3618){
+      setScrollPosition(3618);
+    }else{
+      setScrollPosition(position);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, { passive: true });
+
+    return () => {
+        window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
     <div className='section-component'>
@@ -18,11 +40,11 @@ function Skills() {
                 <div className='progress_inner'>
                   <span>
                     <span className='label opened'>Illustrator</span>
-                    <span className='number'>100%</span>
+                    <span className='number'>{(scrollPosition-3518) + "%"}</span>
                   </span>
                   <div className='background'>
-                    <div className='bar'>
-                      <div className='bar_in' style={{width: 100+ "%", backgroundColor: 'rgb(247, 80, 35)'}}></div>
+                    <div className='bar open'>
+                      <div className='bar_in' style={{width: (scrollPosition-3418) + "%", backgroundColor: 'rgb(247, 80, 35)'}}></div>
                     </div>
                   </div>
                 </div>
