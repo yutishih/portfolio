3ac3e05965bee659514cccc5cf489d8d63a8d2ca
yutishih <yutishih@hannstar.com>
2023-08-15 17:30:54 +0800

    feat: typed anime

---
+++ assets/js/typedEffect.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var typed6 = new Typed('#element', {
+        strings: ['I AM A WEB DESIGNER AND DEVELOPER.^1000 <br> SHARE YOUR IDEAS WITH ME AND WE CAN MAKE THEM COMING TRUE.'],
+        typeSpeed: 40,
+        loop: false
+    });
+});
