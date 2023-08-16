93485d23992f79ddf4eddc864671430f2a0e84e5
yutishih <yutishih@hannstar.com>
2023-08-16 10:17:14 +0800

    feat: typing completed

--- assets/js/typedEffect.js
+++ assets/js/typedEffect.js
@@ -1,7 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var typed6 = new Typed('#element', {
+    var typed1 = new Typed('#indexMainBannerTitle', {
+        strings: ['Hi, This is Yuti.'],
+        typeSpeed: 80,
+        loop: false,
+        onComplete: function (self) {
+            if (self.cursor) {
+                self.cursor.style.display = 'none';
+            }
+        }
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    var typed2 = new Typed('#indexMainBannerSubtitle', {
         strings: ['I AM A WEB DESIGNER AND DEVELOPER.^1000 <br> SHARE YOUR IDEAS WITH ME AND WE CAN MAKE THEM COMING TRUE.'],
         typeSpeed: 40,
-        loop: false
+        loop: false,
+        startDelay: 3000,
+        showCursor: true,
     });
 });
